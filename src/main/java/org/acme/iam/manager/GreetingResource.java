d5afe26ea7b2b3c85c5044a23ad7f2438a4389f2
agmayo <agmayo@minsait.com>
2020-03-26 15:44:39 +0100

    first commit

---
+++ src/main/java/org/acme/iam/manager/GreetingResource.java
@@ -0,0 +1,31 @@
+package org.acme.iam.manager;
+
+import org.jboss.logging.Logger;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
+@Path("/hello")
+public class GreetingResource {
+
+    // Logs to be sent to Logstash
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
+
+    // Metrics to be sent to Logstash
+    @GET
+    @Counted(name = "helloCalls", description = "How many times the /hello resource has been called")
+    @Timed(name = "helloTime", description = "A measure of how long it takes to retrieve a person.", unit = MetricUnits.MILLISECONDS)
+    @Produces(MediaType.TEXT_PLAIN)
+   
+    public String hello() {
+        LOG.debug("Request recieved");
+        LOG.info("Sending 'hello' response");
+        return "hello";
+    }
+}
