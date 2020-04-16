c42774db8e794881185cf88c53b31b7b729caad0
Ramón Prado Moreno <r.pradomoreno@gmail.com>
2020-04-16 15:47:12 +0200

    KeycloakConnectionHealthCheck now uses TokenService

--- src/main/java/org/acme/iam/manager/health/KeycloakConnectionHealthCheck.java
+++ src/main/java/org/acme/iam/manager/health/KeycloakConnectionHealthCheck.java
@@ -1,20 +1,17 @@
 
 package org.acme.iam.manager.health;
 
-import java.util.Base64;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 
-import org.acme.iam.manager.business.TokenServiceInterface;
 import org.acme.iam.manager.business.UserService;
 import org.acme.iam.manager.dto.IamUser;
-import org.acme.iam.manager.dto.TokenData;
 import org.acme.iam.manager.dto.UserToken;
 import org.acme.iam.manager.dto.UserTokenRequest;
+import org.acme.iam.manager.service.TokenService;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
@@ -28,9 +25,9 @@ public class KeycloakConnectionHealthCheck implements HealthCheck {
     @Inject
     @RestClient
     UserService userService;
+    
     @Inject
-    @RestClient
-    TokenServiceInterface tokenServiceInterface;
+    TokenService tokenService;
 
     @Override
     public HealthCheckResponse call() {
@@ -41,7 +38,7 @@ public class KeycloakConnectionHealthCheck implements HealthCheck {
             adminUser.setPassword("Pa55w0rd");
             adminUser.setUsername("admin");
             String realm= "master";
-            UserToken mytoken = buildAdminToken(adminUser.getUsername(),adminUser.getPassword(),realm);
+            UserToken mytoken = tokenService.buildUserToken(adminUser.getUsername(),adminUser.getPassword(),realm);
               
 
     try {
@@ -58,26 +55,6 @@ public class KeycloakConnectionHealthCheck implements HealthCheck {
     return responseBuilder.build();
 
 
-    }
-  
-    private UserToken buildAdminToken(String user, String password, String realm) throws WebApplicationException{
-        String authorizationHeader = "Basic " + Base64.getEncoder().encodeToString("app-authz-rest-springboot:secret".getBytes());
-        TokenData iamTokenInfo = tokenServiceInterface.getToken(realm,
-        "openid-connect",
-        "password",
-        user,
-        password,
-        "admin-cli",
-        MediaType.APPLICATION_JSON,
-        MediaType.APPLICATION_FORM_URLENCODED,
-        authorizationHeader);
-        UserToken tokenData = new UserToken();
-        tokenData.setAccessToken(iamTokenInfo.getAccessToken());
-        tokenData.setRefreshToken(iamTokenInfo.getRefreshToken());
-        tokenData.setAccessTokenExpiration(iamTokenInfo.getExpiresIn());
-        
-        tokenData.setRefreshTokenExpiration(iamTokenInfo.getRefreshExpiration());
-        return tokenData;
     }
 
 }
